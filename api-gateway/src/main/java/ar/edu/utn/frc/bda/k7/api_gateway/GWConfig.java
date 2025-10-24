eb1f7ce7c031d3976eb07dbd7711b03583616c4d
Gabriel Toselli <gabitoselli11@gmail.com>
2025-10-24 16:02:31 -0300

    agregamo primera parte gateway rutas

---
+++ api-gateway/src/main/java/ar/edu/utn/frc/bda/k7/api_gateway/GWConfig.java
@@ -0,0 +1,25 @@
+package ar.edu.utn.frc.bda.k7.api_gateway;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GWConfig {
+    
+    @Bean
+    public RouteLocator buildRutas(RouteLocatorBuilder builder,
+        @Value ("localhost:8181") String puertoRutas,
+        @Value ("/api/ruta/camiones") String uriCamiones,
+        @Value ("/api/ruta/depositos") String uriDepositos,
+        @Value ("/api/ruta/ubicaciones") String uriUbicaciones,
+        @Value ("/api/ruta/tarifas/combustible") String uriTaCombustible,
+        @Value ("/api/ruta/tarifas/volumen") String uriTaVolumen
+        ) {
+        return builder.routes()
+        .route(r -> r.path(uriCamiones).filters(f -> f.rewritePath(uriCamiones, "/camiones")).uri(puertoRutas + uriCamiones))
+        .build();
+    }
+}
