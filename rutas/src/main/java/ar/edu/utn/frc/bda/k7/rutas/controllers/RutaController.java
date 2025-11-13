45a55a7d0b7c8fde4b81a7657987979760f8bcdc
GabiToEstudiantil <403313@sistemas.frc.utn.edu.ar>
2025-11-13 19:58:07 -0300

    no me acuerdo lo poco q no habia commiteado

--- rutas/src/main/java/ar/edu/utn/frc/bda/k7/rutas/controllers/RutaController.java
+++ rutas/src/main/java/ar/edu/utn/frc/bda/k7/rutas/controllers/RutaController.java
@@ -3,11 +3,24 @@ package ar.edu.utn.frc.bda.k7.rutas.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ar.edu.utn.frc.bda.k7.rutas.services.RutaService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/ruta")
 public class RutaController {
+
+    private final RutaService rutaService;
+
+    @PostMapping("/estimas-costo")
+    public Double estimarCosto(@RequestBody String peso, String volumen, String idOrigen, String idDestino) {
+        Class <?> resCamino = rutaService.construirRuta(Integer.parseInt(idOrigen), Integer.parseInt(idDestino));
+
+        return 0.0;
+    }
     
 }
--- rutas/src/main/java/ar/edu/utn/frc/bda/k7/rutas/services/RutaService.java
+++ rutas/src/main/java/ar/edu/utn/frc/bda/k7/rutas/services/RutaService.java
@@ -2,10 +2,26 @@ package ar.edu.utn.frc.bda.k7.rutas.services;
 
 import org.springframework.stereotype.Service;
 
+import ar.edu.utn.frc.bda.k7.rutas.dtos.UbicacionDTO;
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 public class RutaService {
 
+    private final UbicacionService ubicacionService;
+    private final TarifaCombustibleService tarifaCombustibleService;
+
+    public Class<?> construirRuta(Integer idOrigen, Integer idDestino) {
+        UbicacionDTO origen = ubicacionService.getUbicacionById(idOrigen);
+        UbicacionDTO destino = ubicacionService.getUbicacionById(idDestino);
+        this.llamarMsMaps(origen, destino);
+        return Object.class;
+    }
+
+    private String llamarMsMaps(UbicacionDTO origen, UbicacionDTO destino) {
+        // Lógica para llamar al microservicio de Maps y obtener la ruta
+        return "";
+        // Por simplicidad, retornamos una cadena simulada
+    }
 }
