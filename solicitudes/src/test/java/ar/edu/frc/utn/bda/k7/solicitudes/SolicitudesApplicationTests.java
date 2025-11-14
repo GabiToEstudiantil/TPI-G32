d1291866612e64115d8a29f414df3310d0848d64
Sofía <401547@sistemas.frc.utn.edu.ar>
2025-11-14 12:08:35 -0300

    Agrgeo estructura de solicitudes

---
+++ solicitudes/src/test/java/ar/edu/frc/utn/bda/k7/solicitudes/SolicitudesApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.frc.utn.bda.k7.solicitudes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolicitudesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
